7afad3bbf0d9bbd1caccf94382c7d093e28955f4
Illera1999 <illerarodriguez1999@gmail.com>
2025-07-30 20:19:54 +0100

    Spacer

--- MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/components/layout/RetoUno.kt
+++ MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/components/layout/RetoUno.kt
@@ -4,16 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Preview(showBackground = true)
@@ -29,6 +30,7 @@ fun RetoUno(modifier: Modifier = Modifier) {
         ) {
             Text(text = "Ejemplo 1")
         }
+        Spacer(modifier = Modifier.height(20.dp))
         Row(
             modifier = Modifier.weight(1f)
         ) {
@@ -52,6 +54,7 @@ fun RetoUno(modifier: Modifier = Modifier) {
                 Text(text = "Ejemplo 3")
             }
         }
+        Spacer(modifier = Modifier.height(20.dp))
         Box(
             modifier = Modifier
                 .weight(1f)
