9d63adce01d7dbbf5543ea39a2e30f3da1a6abb6
Illera1999 <illerarodriguez1999@gmail.com>
2025-08-04 20:10:30 +0100

    Estados, tratar de reducir las funciones

--- MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/state/MyState.kt
+++ MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/state/MyState.kt
@@ -1,15 +1,32 @@
 package com.illeradevs.myfirstcomposeapp.state
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun MyState(modifier: Modifier = Modifier) {
-    var number = remember{ mutableStateOf(0)}
-    Text(text = "Púlsame: ${number.value}", modifier = modifier.clickable { number.value += 1 })
+    var number by rememberSaveable { mutableStateOf(0) }
+    Column(modifier = modifier) {
+        StateExample(number) { number += 1  }
+        StateExample2(number = number, onClick = { number += 1  })
+    }
+}
+
+
+@Composable
+fun StateExample(number: Int, onClick:() -> Unit){
+    Text(text = "Púlsame: $number", modifier = Modifier.clickable { onClick() })
+}
+
+@Composable
+fun StateExample2(number: Int, onClick:() -> Unit){
+    Text(text = "Púlsame: $number", modifier = Modifier.clickable { onClick() })
 }
