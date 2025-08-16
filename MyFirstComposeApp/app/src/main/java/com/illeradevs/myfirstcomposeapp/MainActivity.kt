10c107bb30f8b522f1ae9d7cdaf65beec9790353
Illera1999 <illerarodriguez1999@gmail.com>
2025-08-16 14:18:17 +0100

    Imahenes primeros pasos

--- MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/MainActivity.kt
+++ MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/MainActivity.kt
@@ -8,8 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.illeradevs.myfirstcomposeapp.components.MyButton
-import com.illeradevs.myfirstcomposeapp.components.MyTextFieldParent
+import com.illeradevs.myfirstcomposeapp.components.MyImage
 import com.illeradevs.myfirstcomposeapp.ui.theme.MyFirstComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstComposeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyButton(modifier = Modifier.padding(innerPadding))
+                    MyImage(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
--- MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/components/Image.kt
+++ MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/components/Image.kt
@@ -1,2 +1,27 @@
 package com.illeradevs.myfirstcomposeapp.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.illeradevs.myfirstcomposeapp.R
+
+
+@Preview(showBackground = true)
+@Composable
+fun MyImage(modifier: Modifier = Modifier) {
+   Image(
+       painter = painterResource(id = R.drawable.payaso),
+       contentDescription = "Imagen",
+       modifier = Modifier.height(200.dp).width(100.dp),
+       contentScale = ContentScale.FillWidth
+
+
+   )
+}
