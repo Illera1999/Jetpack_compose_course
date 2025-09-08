fae1db4cb95f438b0e8d0e9e969f7ebb89cbaec2
Illera1999 <illerarodriguez1999@gmail.com>
2025-09-08 18:11:57 +0100

    Slider con dos thumb

--- MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/components/Sliders.kt
+++ MyFirstComposeApp/app/src/main/java/com/illeradevs/myfirstcomposeapp/components/Sliders.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.RangeSlider
+import androidx.compose.material3.RangeSliderState
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderColors
 import androidx.compose.material3.SliderDefaults
@@ -18,7 +22,9 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
@@ -67,16 +73,72 @@ fun MySliderAdvance(modifier: Modifier = Modifier) {
             colors = color,
             thumb = { state: SliderState ->
                 Box(
-                    Modifier.width(10.dp).height(30.dp).background(Color.Red)
+                    Modifier
+                        .width(10.dp)
+                        .height(30.dp)
+                        .background(Color.Red)
                 )
             },
             track = { state: SliderState ->
                 Box(
-                    Modifier.width(100.dp).height(5.dp).background(Color.Blue)
+                    Modifier
+                        .width(100.dp)
+                        .height(5.dp)
+                        .background(Color.Blue)
                 )
             },
         )
         Text(state.value.toString())
         Text(example)
     }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyRangeSlider(modifier: Modifier = Modifier) {
+    val color: SliderColors = SliderDefaults.colors(
+        thumbColor = Color.Red,
+        activeTrackColor = Color.Yellow,
+        inactiveTrackColor = Color.Blue,
+        disabledThumbColor = Color.Cyan,
+        disabledActiveTrackColor = Color.Magenta,
+        disabledInactiveTrackColor = Color.Green,
+    )
+    val state: RangeSliderState = remember {
+        RangeSliderState(
+            activeRangeStart = 3f,
+            activeRangeEnd = 6f,
+            valueRange = 0f..10f,
+            steps = 9,
+        )
+    }
+    Column(modifier = modifier.padding(horizontal = 30.dp)) {
+        RangeSlider(
+            state = state, colors = color,
+            startThumb = {
+                Box(
+                    Modifier.size(30.dp)
+                        .clip(CircleShape)
+                        .background(Color.Green),
+                    contentAlignment = Alignment.Center
+                ) {
+//                    Text(it.activeRangeStart.toString())
+                    Text(String.format("%.1f", it.activeRangeStart))
+                }
+            },
+            endThumb = {
+                Box(
+                    Modifier.size(30.dp)
+                        .clip(CircleShape)
+                        .background(Color.Blue),
+                    contentAlignment = Alignment.Center
+                ) {
+//                    Text(it.activeRangeStart.toString())
+                    Text(String.format("%.1f", it.activeRangeStart))
+                }
+            },
+        )
+    }
+
 }
